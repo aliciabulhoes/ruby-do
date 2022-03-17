7883cf68df0c53d5c5ce42866b82db41c0018f7c
aliciabulhoes <aliciabulhoes@gmail.com>
2022-03-17 16:10:57 -0400

    Get list by id endpoint

--- app/api/ruby_do/v1/lists.rb
+++ app/api/ruby_do/v1/lists.rb
@@ -2,8 +2,19 @@ module RubyDo
   module V1
     class Lists < Grape::API
       resource :lists do
+        desc 'Return all lists'
         get do
           List.all
+        end
+
+        desc 'Return a specific list.'
+        params do
+          requires :id, type: Integer, desc: 'List ID.'
+        end
+        route_param :id do
+          get do
+            List.find(params[:id])
+          end
         end
       end
     end
--- spec/api/ruby_do/v1/lists_spec.rb
+++ spec/api/ruby_do/v1/lists_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe RubyDo::V1::Lists, type: :request do
   describe 'get /api/v1/lists' do
-    subject(:get_path) { get '/api/v1/lists'}
+    subject(:get_path) { get '/api/v1/lists' }
     let!(:lists) { FactoryBot.create_list(:list, 2) }
 
     it 'returns success' do
@@ -15,6 +15,25 @@ describe RubyDo::V1::Lists, type: :request do
       # with one list ie. let!(:list) { FactoryBot.create(:list) }
       # expect(JSON.parse(response.body)[0]).to eq(JSON.parse(lists.to_json))
       expect(JSON.parse(response.body)).to eq(JSON.parse(lists.to_json))
+    end
+  end
+
+  describe 'get /api/v1/lists/:id' do
+    subject(:get_path) { get "/api/v1/lists/#{id}" }
+    let!(:list) { FactoryBot.create(:list) }
+    context 'invalid params' do
+      let(:id) { 1000 }
+      it 'raises error' do
+        expect { get_path }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'valid params' do
+      let(:id) { list.id }
+      it 'returns success' do
+        get_path
+        expect(JSON.parse(response.body)).to eq(JSON.parse(list.to_json))
+      end
     end
   end
 end
